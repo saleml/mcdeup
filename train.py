fc64de7114d79a3dff3c925651b2f80edb15b102
saleml <salemlahlou9@gmail.com>
2021-04-21 14:45:39 -0400

    train_deup

--- train.py
+++ train.py
@@ -4,9 +4,7 @@ from torch.utils.data import TensorDataset, DataLoader
 import numpy as np
 
 
-def train_main_model(model, train_data, validation_data, epochs=500, lr=1e-3, batch_size=32):
-    optimizer = torch.optim.Adam([param for name, param in model.named_parameters() if 'dropout' not in name],
-                                 lr=lr)
+def train_main_model(model, optimizer, train_data, validation_data, epochs=500, batch_size=32):
     loss_fn = nn.MSELoss()
 
     model.train()
@@ -26,3 +24,33 @@ def train_main_model(model, train_data, validation_data, epochs=500, lr=1e-3, ba
         losses.append(np.sum(epoch_losses) / train_data[:][0].shape[0])
         valid_losses.append(loss_fn(model(validation_data[:][0]), validation_data[:][1]))
     return losses, valid_losses
+
+
+def train_deup(model, optimizer, full_data, epochs=500, batch_size=32, n_samples=5):
+    loss_fn = nn.MSELoss()
+
+    loader = DataLoader(full_data, shuffle=True, batch_size=batch_size)
+    losses = []
+
+    for epoch in range(epochs):
+
+        epoch_losses = []
+        for batch_id, (xi, yi) in enumerate(loader):
+            optimizer.zero_grad()
+            model.eval()
+            y_mean = model(xi)
+            model.train()
+            y_noisy = torch.cat([model(xi) for _ in range(n_samples)], 1)
+            pred_uncertainties = ((y_mean - y_noisy) ** 2)
+            pred_uncertainty = pred_uncertainties.mean(1).unsqueeze(-1)
+            variance_estimator = (n_samples / n_samples - 1) * torch.var(pred_uncertainties, 1)
+
+            true_uncertainty = (yi - y_mean) ** 2
+            f_loss = loss_fn(pred_uncertainty, true_uncertainty) - (n_samples + 1) / n_samples * variance_estimator.mean()
+
+            epoch_losses.append(f_loss.item() * xi.shape[0])
+            f_loss.backward()
+            optimizer.step()
+
+        losses.append(np.sum(epoch_losses) / full_data[:][0].shape[0])
+    return losses

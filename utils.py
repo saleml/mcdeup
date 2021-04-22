4f9d5c649d70728ad2abb956ddc188f9a020bd6e
saleml <salemlahlou9@gmail.com>
2021-04-22 00:37:44 -0400

    Add inverse sigmoid function
    Better plots
    
    Make explicit whether we are using parametric noise

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 import matplotlib.pyplot as plt
 from torch.distributions.exponential import Exponential
 from torch import sigmoid
@@ -32,6 +33,10 @@ class DifferentiableBernoulli:
         return hard_samples - soft_samples.detach() + soft_samples
 
 
+def inv_sigmoid(p):
+    return np.log(p / (1 - p))
+
+
 def get_dist(model, x, num_samples=10):
     model.train()
     outputs = torch.cat([model(x).unsqueeze(0) for _ in range(num_samples)])
@@ -40,35 +45,37 @@ def get_dist(model, x, num_samples=10):
     return y_mean.detach().squeeze(), y_std.detach().squeeze()
 
 
-def eval_error(model_drop, X, Y, std=False):
-    model_drop.eval()
-    y_mean = model_drop(X)
-    model_drop.train()
+def eval_error(model, X, Y, n_samples=1, parametric_noise=False, x_input=False):
+    model.eval()
+    y_mean = model(X)
+    model.train()
 
-    if std:
-        y_noisy = torch.cat([model_drop(X) for _ in range(10)], 1)
-        pred_uncertainty = y_noisy.var(dim=1).squeeze()
-    else:
-        y_noisy = model_drop(X)
-        pred_uncertainty = (y_mean - y_noisy) ** 2
+    y_noisy = torch.cat([model(X, parametric_noise=parametric_noise, noise_input=X if x_input else None)
+                                 for _ in range(n_samples)], 1)
+    pred_uncertainties = ((y_mean - y_noisy) ** 2)
+    pred_uncertainty = pred_uncertainties.mean(1).unsqueeze(-1)
+
     true_uncertainty = (Y - y_mean) ** 2
     return ((pred_uncertainty - true_uncertainty) ** 2).mean()
 
 
-def get_dist_deup(model_drop, x):
+def get_dist_deup(model_drop, x, n_samples=1, parametric_noise=False, x_input=False):
     model_drop.eval()
     y_mean = model_drop(x)
     model_drop.train()
-    y_noisy = model_drop(x)
-    y_std = torch.abs(y_mean - y_noisy)
+    y_noisy = torch.cat([model_drop(x, parametric_noise=parametric_noise, noise_input=x if x_input else None)
+                         for _ in range(n_samples)], 1)
+    y_std = torch.abs(y_mean - y_noisy).mean(1)
     return y_mean.detach().squeeze(), y_std.detach().squeeze()
 
 
-def evaluate_and_plot(model, x_test, full_X, full_Y, oos_ood=None, test_data=None, deup=False):
+def evaluate_and_plot(model, x_test, full_X, full_Y, oos_ood=None, test_data=None, deup=False, parametric_noise=False,
+                      x_input=False, n_samples=1):
+    plt.figure(figsize=(15, 5))
     model.eval()
     non_dropout_pred = model(x_test).detach()
     if deup:
-        Y_mean, Y_std = get_dist_deup(model, x_test)
+        Y_mean, Y_std = get_dist_deup(model, x_test, n_samples=n_samples, parametric_noise=parametric_noise, x_input=x_input)
     else:
         Y_mean, Y_std = get_dist(model, x_test, num_samples=100)
     plt.plot(full_X, full_Y, 'r.', label='train_data')

5b5e6d47563344fa7c1aedc4f6669abe88427252
saleml <salemlahlou9@gmail.com>
2021-05-16 19:19:03 -0400

    training regular deup and deup plus plus, with empirical covariances (rank 1 kernel)

--- train.py
+++ train.py
@@ -57,3 +57,62 @@ def train_deup(model, optimizer, full_data, epochs=500, batch_size=32, n_samples
 
         losses.append(np.sum(epoch_losses) / full_data[:][0].shape[0])
     return losses, callbacks
+
+
+def train_regular_deup(model, deup_model, optimizer, full_data, epochs=500, batch_size=32, callback=None):
+    loss_fn = nn.MSELoss()
+
+    loader = DataLoader(full_data, shuffle=True, batch_size=batch_size)
+    losses = []
+    callbacks = []
+
+    for epoch in range(epochs):
+        if callback is not None:
+            callbacks.append(callback(model))
+        epoch_losses = []
+        for batch_id, (xi, yi) in enumerate(loader):
+            optimizer.zero_grad()
+            model.eval()
+            y_mean = model(xi)
+            true_uncertainty = (yi - y_mean) ** 2
+
+            pred_uncertainty = deup_model(xi).pow(2).sum(1, keepdims=True)  # deup_model outputs N x d tensors, this transforms it to N x 1
+            deup_model.train()
+
+            f_loss = loss_fn(pred_uncertainty, true_uncertainty)
+
+            epoch_losses.append(f_loss.item() * xi.shape[0])
+            f_loss.backward()
+            optimizer.step()
+
+        losses.append(np.sum(epoch_losses) / full_data[:][0].shape[0])
+    return losses, callbacks
+
+
+def train_regular_deup_plusplus(model, deup_model, optimizer, full_data, epochs=500, batch_size=32, callback=None):
+    loss_fn = nn.MSELoss()
+    loader = DataLoader(full_data, shuffle=True, batch_size=batch_size)
+    losses = []
+    callbacks = []
+
+    for epoch in range(epochs):
+        if callback is not None:
+            callbacks.append(callback(model))
+        epoch_losses = []
+        for batch_id, (xi, yi) in enumerate(loader):
+            optimizer.zero_grad()
+            model.eval()
+            y_mean = model(xi)
+            differences = (yi - y_mean).squeeze()  # should be a 1D array
+            empirical_covariance = torch.outer(differences, differences)
+            pred_differences = deup_model(xi).squeeze()  # should be a 1D array
+            pred_covariance = torch.outer(pred_differences, pred_differences)
+
+            # f_loss = (empirical_covariance - pred_covariance).pow(2).diag().mean()
+            f_loss = (empirical_covariance - pred_covariance).pow(2).mean()
+            epoch_losses.append(f_loss.item() * xi.shape[0])
+            f_loss.backward()
+            optimizer.step()
+
+        losses.append(np.sum(epoch_losses) / full_data[:][0].shape[0])
+    return losses, callbacks

a787efbfcecd15f4b0fa9cb0126bb63504e59d7d
saleml <salemlahlou9@gmail.com>
2021-05-19 13:58:45 -0400

    Add AdditiveNoisyNet

--- networks.py
+++ networks.py
@@ -1,11 +1,13 @@
 import torch.nn as nn
 
-from dropout import (Dropout,
-                     RegularDropout, RegularDropoutPerLayer,
-                     MultiplicativeGaussian, MultiplicativeGaussianPerLayer)
+from old.dropout import (Dropout,
+                         RegularDropout, RegularDropoutPerLayer,
+                         MultiplicativeGaussianPerLayer)
+from noise_generator import GaussianNoiseGenerator
+from noise_transformer import ConstantNoiseTransformer
 
 
-class Model(nn.Module):
+class DropoutModel(nn.Module):
     def __init__(self, input_dim=1, n_hidden=32, hidden_layers=1, output_dim=1, dropout_module=RegularDropout, **kwargs):
         super().__init__()
 
@@ -26,6 +28,49 @@ class Model(nn.Module):
         for module in self.model:
             if isinstance(module, Dropout):
                 out = module(out, **kwargs)
+            else:
+                out = module(out)
+        return out
+
+
+class AdditiveNoisyNet(nn.Module):
+    def __init__(self, input_dim=1, n_hidden=32, hidden_layers=1,
+                 output_dim=1, activation_fn=nn.ReLU, noise_generator=None,
+                 noise_transformer=None):
+        super().__init__()
+
+        model = nn.Sequential()
+        if noise_generator is None:
+            noise_generator = GaussianNoiseGenerator(1)
+        self.noise_generator = noise_generator
+        if noise_transformer is None:
+            noise_transformer = ConstantNoiseTransformer(output_dim + n_hidden * (hidden_layers - 1))
+        self.noise_transformer = noise_transformer
+
+        self.n_hidden = n_hidden
+
+        model.add_module('input_layer', nn.Linear(input_dim, n_hidden))
+        model.add_module('activation0', activation_fn())
+        for i in range(hidden_layers - 1):
+            model.add_module('hidden_layer{}'.format(i + 1), nn.Linear(n_hidden, n_hidden))
+            model.add_module('activation{}'.format(i + 1), activation_fn())
+        model.add_module('output_layer', nn.Linear(n_hidden, output_dim))
+        self.model = model
+
+    def forward(self, x, sigma=1.):
+        z = self.noise_generator.sample(x)
+        n = self.noise_transformer(z)  # should be of shape B x n_units
+        out = x
+        i = 0
+        for (name, module) in self.model.named_modules():
+            if 'hidden_layer' in name:
+                out = module(out)
+                out += sigma * n[:, i * self.n_hidden:(i + 1) * self.n_hidden]
+                i += 1
+            elif name == 'output_layer':
+                out = module(out)
+                # print(out.shape, n[:, i * self.n_hidden:].shape)
+                out += sigma * n[:, i * self.n_hidden:]
             else:
                 out = module(out)
         return out

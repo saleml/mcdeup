d51f6348e46c92d4134ea2f9930bed7ef3c384ea
saleml <salemlahlou9@gmail.com>
2021-08-05 15:43:05 -0400

    update networks py file

--- networks.py
+++ networks.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 from abc import ABC, abstractmethod
 import torch.nn.functional as F
+import numpy as np
+import torch
 
 
 class NoisyNetwork(nn.Module, ABC):
@@ -79,4 +81,73 @@ class MultiplicativeNetwork(NoisyNetwork):
         out = self.hidden_layer_2(out) * noise_2
         out = self.output_net(out)
         return out
+
+
+class OnePlusId(nn.Module):
+    def forward(self, x):
+        return 1. + x
+
+
+class NoiseNet(nn.Module):
+    def __init__(self, noise_dim=64, depth=2, width=256, output_dim=256, use_x=False, x_dim=1, final_layer=None):
+        super().__init__()
+        self.depth = depth
+        self.use_x = use_x
+        if use_x:
+            self.x_layer = nn.Linear(x_dim, width // 2)
+        self.noise_layer = nn.Linear(noise_dim,
+                                     width - width // 2 if use_x else width,
+                                     bias=False)  # why no bias
+        # self.x_layer = nn.Linear(1, width, bias=True)
+        self.hidden_layers = nn.ModuleList([nn.Linear(width, width) for _ in range(depth)])
+        self.output_layer = nn.Linear(width, output_dim, bias=False)  # why no bias ?
+        if final_layer is None:
+            self.final_layer = OnePlusId()
+        elif final_layer == 'softplus':
+            self.final_layer = nn.Softplus(beta=np.log(2))
+        else:
+            raise NotImplementedError('No final layer with this name')
+
+    def forward(self, x, z):
+        if z is None:
+            return 1
+        # the x input is if we want this noisy part of the network to have x as input as well
+        # out = F.relu(self.x_layer(x))
+        # out = F.relu(self.hidden_layer_1(out)) * self.noise_layer(noise)
+        out = F.relu(self.noise_layer(z))
+        if self.use_x:
+            x_hid = F.relu(self.x_layer(x))
+            if x_hid.ndim == 2 and z.ndim == 3:
+                x_hid = x_hid.repeat(z.shape[0], 1, 1)
+            out = torch.cat([out, x_hid], -1)
+        for i in range(self.depth):
+            out = F.relu(self.hidden_layers[i](out))
+        out = self.output_layer(out)
+        return self.final_layer(out)
+
+
+class MultiplicativeNetworkTmp(NoisyNetwork):
+    def __init__(self, input_dim=1, output_dim=1, depth=2, width=256, noise_dim=64, noise_depth=2, noise_width=256,
+                 bottleneck_ratio=2, use_x=False, noise_final_layer=None):
+        super().__init__()
+        self.depth = depth
+        self.x_layer = nn.Linear(input_dim, width)
+        self.hidden_layers = nn.ModuleList([nn.Linear(width, width) for _ in range(depth)])
+        self.noise_nets = nn.ModuleList([NoiseNet(noise_dim, depth=noise_depth,
+                                                  width=noise_width, output_dim=width,
+                                                  final_layer=noise_final_layer, use_x=use_x) for _ in range(depth)])
+        self.output_net = nn.Sequential(nn.Linear(width, width // bottleneck_ratio),
+                                        nn.ReLU(),
+                                        nn.Linear(width // bottleneck_ratio, output_dim))
+        self.noise_dim = noise_dim
+
+    def forward(self, x, z=None):
+        noises = [self.noise_nets[i](x, z) for i in range(self.depth)]
+        out = F.relu(self.x_layer(x))
+        for i in range(self.depth):
+            out = F.relu(self.hidden_layers[i](out)) * noises[i]
+        # out = self.hidden_layers[-1](out) * noises[-1]  # why no relu here ?
+        out = self.output_net(out)
+        return out
+
 
